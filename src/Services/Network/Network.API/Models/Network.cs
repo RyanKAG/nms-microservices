6e5c063a66dea470f56b73d543f230390e5faf60
ryankag <rayanalgh@gmail.com>
2021-06-27 16:04:33 +0300

    fixed project structure

---
+++ src/Services/Network/Network.API/Models/Network.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Network.API.Models
+{
+    public class Network : IModel
+    {
+        public Guid Id { get; set; }
+        public string Ip { get; set; }
+        
+    }
+}
